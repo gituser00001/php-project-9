e415a3eaf670ef505519a2bc71ace6cc39d2abce
gituser00001 <lptvsergey@yandex.ru>
2023-01-28 16:07:18 +0300

    new router

--- public/index.php
+++ public/index.php
@@ -7,19 +7,51 @@ use Slim\Factory\AppFactory;
 use DI\Container;
 use PageAnalyzer\Database\Repository;
 
+$sessionPath = __DIR__ . '/../temp/sessions/';
+session_save_path($sessionPath);
+// Старт PHP сессии
+session_start();
+
 //$test = new Repository();
 //$t = $test->insertUrl('test12.com');
 
+// Установка зависимостей в контейнер
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
 });
 
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
+// Home Page
 $app->get('/', function ($request, $response) {
-    $params = ['test' => 'PageAnalyzer Demo!'];
+
+    $messages = $this->get('flash')->getMessages();
+    $params = [
+        'url' => [],
+        'errors' => $messages
+    ];
     return $this->get('renderer')->render($response, 'index.phtml', $params);
-});
+})->setName('homepage');
+// Получаем роутер – объект отвечающий за хранение и обработку  именнованых маршрутов
+$router = $app->getRouteCollector()->getRouteParser();
+
+// Добавление url
+$app->post('/urls', function ($request, $response) use ($router) {
+    $url = $request->getParsedBodyParam('url');
+    // Валидация url
+    $v = new Valitron\Validator($url);
+    $v->rule('url', 'name');
+    $v->validate();
+    $errors = $v->errors();
+    $params = ['url' => $url, 'errors' => 'noerros'];
+    return $this->get('renderer')->render($response->withStatus(422), 'index.phtml', $params);
+})->setName('addUrl');
+
 $app->run();
