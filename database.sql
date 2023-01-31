c0d5043c8788180ef36647de8d7cf34209b229d4
gituser00001 <lptvsergey@yandex.ru>
2023-01-31 14:55:30 +0300

    add table url_checks

--- database.sql
+++ database.sql
@@ -3,3 +3,12 @@ CREATE TABLE urls (
     name varchar(255) UNIQUE NOT NULL,
     created_at timestamp 
 );
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code int,
+    h1 text,
+    title text,
+    description text,
+    created_at timestamp
+);
