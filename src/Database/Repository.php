26ff1b883799b44867d3c2c0976aeb9de7823ace
gituser00001 <lptvsergey@yandex.ru>
2023-01-31 23:05:44 +0300

    addCheck updated

--- src/Database/Repository.php
+++ src/Database/Repository.php
@@ -59,15 +59,19 @@ class Repository
         return $result;
     }
 
-    public function addCheck($id, $statusCode)
+    public function addCheck($id, $statusCode, $title = '', $h1 = '', $description = '')
     {
         // подготовка запроса для добавления данных
         $created_at = Carbon::now();
-        $sql = 'INSERT INTO url_checks (url_id, created_at, status_code) VALUES (:url_id, :created_at, :status_code)';
+        $sql = 'INSERT INTO url_checks (url_id, status_code, h1, title, description, created_at)
+        VALUES (:url_id, :status_code, :h1, :title, :description, :created_at)';
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(':url_id', $id);
         $stmt->bindValue(':created_at', $created_at);
         $stmt->bindValue(':status_code', $statusCode);
+        $stmt->bindValue(':h1', $h1);
+        $stmt->bindValue(':title', $title);
+        $stmt->bindValue(':description', $description);
 
         $stmt->execute();
 
@@ -77,7 +81,7 @@ class Repository
 
     public function findCheckUrl($id)
     {
-        $sql ='SELECT id, created_at, status_code FROM url_checks WHERE url_id = :id';
+        $sql ='SELECT * FROM url_checks WHERE url_id = :id';
         $stmt = $this->db->prepare($sql);
         $stmt->bindValue(':id', $id);
         $stmt->execute();
